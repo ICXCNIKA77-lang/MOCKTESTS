55bc677076e7e3ed5cdb12ac0af2e4aca402ad96
ICXCNIKA77-lang <giventowana.manda@gmail.com>
2026-09-21 09:56:14 +0200

    Add files via upload

---
+++ testp2/src/Staff.java
@@ -0,0 +1,13 @@
+public  abstract class Staff {
+    public static void  main (String[] args){
+        int StaffNumber;
+        String StaffLocation;
+
+        public class Staff (int StaffNUmber, String StaffLocation){
+            int StaffNUmber = this.StaffNUmber;
+            String StaffLocation = this.StaffLocation;
+        }
+
+
+    }
+}
---
+++ testp2/src/iStaff.java
@@ -0,0 +1,3 @@
+public interface iStaff {
+    public int getStaffN
+}
